f2542b7145b80d7c3c8ccd5e4af01e3c40936896
yuan8ba <41765558+yuan8ba@users.noreply.github.com>
2023-05-26 22:34:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,18 @@ source /app/config.sh
 service ssh start
 service nginx start
 
+
+mkdir /etc/v2ray /usr/local/v2ray
+unzip -d /usr/local/v2ray v2ray-linux-64.zip
+cp geosite.dat /usr/local/v2ray
+cp geoip.dat /usr/local/v2ray
+cp config.json /etc/v2ray
+cp nginx.conf /etc/nginx/nginx.conf
+/usr/sbin/nginx -s reload
+nohup /usr/local/v2ray/v2ray -config=/etc/v2ray/config.json >/app/v.log 2>&1 &
+
+
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
-/app/ini.sh
 ngrok tcp 22 --log=stdout > ngrok.log
